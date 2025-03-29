098f655163cd3452c11df4fbe49d62f725ebb174
Joseph Nishimwe <j.nishimwe@alustudent.com>
2025-03-29 10:19:49 +0200

    0-the_sky_is_the_limit_not.pp

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,21 @@
-# puppet that increases the ULIMIT of nginx default from 15 to 4096
+# This Puppet script increases the ULIMIT of nginx default file to handle more concurrent connections
+# It modifies both the soft and hard limits for the nginx process
 
-#increase ULIMIT
+# Increase ULIMIT in nginx default configuration
 exec { 'increase-ulimit':
-    command => '/bin/sed -i \'s/ULIMIT="-n 15"/ULIMIT="-n 4096"/\' /etc/default/nginx',
+  command => '/bin/sed -i \'s/ULIMIT="-n 15"/ULIMIT="-n 4096"/\' /etc/default/nginx',
+  notify  => Exec['set-hard-limit'],
 }
 
-#restart nginx
+# Increase hard limit for nginx processes
+exec { 'set-hard-limit':
+  command     => '/bin/sed -i \'1s/^/nginx\tsoft\tnofile\t4096\nnginx\thard\tnofile\t4096\n/\' /etc/security/limits.conf',
+  refreshonly => true,
+  notify      => Exec['restart-nginx'],
+}
+
+# Restart nginx to apply changes
 exec { 'restart-nginx':
-    command => '/usr/sbin/service nginx restart'
+  command     => '/usr/sbin/service nginx restart',
+  refreshonly => true,
 }
